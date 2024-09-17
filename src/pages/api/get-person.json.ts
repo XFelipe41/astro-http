eb52e4a951ec0651c57d9ed41ced727d47b8d6f9
XFelipe41 <jfcaicedot@ut.edu.co>
2024-09-17 15:42:00 -0500

    Se agrega el Proyecto

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const person = {
+    name: "felipe caicedo",
+    age: 25,
+  };
+  return new Response(JSON.stringify(person), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
